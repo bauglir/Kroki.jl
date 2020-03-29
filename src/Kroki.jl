f1e909c0c2b00899b2d7f877c59b40af2bab8db3
Joris Kraak <me@joriskraak.nl>
2020-03-29 18:11:27 +0200

    feat(Diagram): add canonical diagram representation
    This type is the basis for all other functionality of the package,
    capturing the different types of diagrams Kroki can render and their
    specifications.

--- src/Kroki.jl
+++ src/Kroki.jl
@@ -1,2 +1,30 @@
 module Kroki
+
+"""
+A representation of a diagram that can be rendered by a Kroki service. The
+specification of the diagram type, which is captured as a `Symbol` in the
+type's parameter, is case-insensitive!
+
+# Examples
+
+```
+Diagram{Val{:PlantUML}}("Kroki -> Julia: Hello Julia!")
+```
+"""
+struct Diagram{Val}
+  "The textual specification of the diagram"
+  specification::AbstractString
+
+  function Diagram{T}(specification::AbstractString) where T <: Val
+    @assert T.parameters[1] isa Symbol
+    new(specification)
+  end
+end
+
+"""
+Shorthand for constructing a [`Diagram`](@ref) without having to specify its
+parametric part.
+"""
+Diagram(type::Symbol, specification::AbstractString) = Diagram{Val{type}}(specification)
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,23 @@
 module KrokiTest
 
-using Test: @testset
+using Test: @testset, @test, @test_nowarn, @test_throws
+
+using Kroki: Diagram
 
 @testset "Kroki" begin
+  @testset "`Diagram`" begin
+    @testset "requires `Symbol` parameter" begin
+      @test_nowarn Diagram{Val{:Foo}}("")
+      @test_throws(AssertionError, Diagram{Val{1}}(""))
+    end
+
+    @testset "shorthand automatically parameterizes type" begin
+      content = "A -> B: C"
+      type = :PlantUML
+
+      @test Diagram(type, content) === Diagram{Val{type}}(content)
+    end
+  end
 end
 
 end
