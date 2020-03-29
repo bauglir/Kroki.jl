f04b1b369835393ec87c103db7017c59c4ee7763
Joris Kraak <me@joriskraak.nl>
2020-03-29 22:32:27 +0200

    feat(Diagram): add `render`ing through publicly hosted service
    The `render` method takes `Diagram`s, sends them to the publicly hosted
    Kroki service (https://kroki.io) and returns the corresponding
    representation for the selected output format if available.

--- src/Kroki.jl
+++ src/Kroki.jl
@@ -1,5 +1,9 @@
 module Kroki
 
+using Base64: base64encode
+using CodecZlib: ZlibCompressor, transcode
+using HTTP: request
+
 """
 A representation of a diagram that can be rendered by a Kroki service. The
 specification of the diagram type, which is captured as a `Symbol` in the
@@ -26,5 +30,32 @@ Shorthand for constructing a [`Diagram`](@ref) without having to specify its
 parametric part.
 """
 Diagram(type::Symbol, specification::AbstractString) = Diagram{Val{type}}(specification)
+
+"""
+Compresses a [`Diagram`](@ref)'s `specification` using
+[zlib](https://zlib.net), turning the resulting bytes into a URL-safe Base64
+encoded payload (i.e. replacing `+` by `-` and `/` by `_`) to be used in
+communication with a Kroki service.
+
+See https://docs.kroki.io/kroki/setup/encode-diagram/ for more information.
+"""
+UriSafeBase64Payload(diagram::Diagram) = foldl(
+  replace, [ '+' => '-', '/' => '_'];
+  init = base64encode(transcode(ZlibCompressor, diagram.specification))
+)
+
+"""
+Renders a [`Diagram`](@ref) through a Kroki service to the specified output
+format.
+"""
+render(diagram::Diagram{T}, output_format::AbstractString) where T <: Val = getfield(
+  request("GET", join([
+    "https://kroki.io",
+    lowercase("$(T.parameters[1])"),
+    output_format,
+    UriSafeBase64Payload(diagram)
+  ], '/')),
+  :body
+)
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ module KrokiTest
 
 using Test: @testset, @test, @test_nowarn, @test_throws
 
-using Kroki: Diagram
+using Kroki: Diagram, render
 
 @testset "Kroki" begin
   @testset "`Diagram`" begin
@@ -16,6 +16,48 @@ using Kroki: Diagram
       type = :PlantUML
 
       @test Diagram(type, content) === Diagram{Val{type}}(content)
+    end
+  end
+
+  @testset "`render`" begin
+    # This is not an exhaustive list of supported diagram types or output
+    # formats, but serves to verify generic rendering logic is available for at
+    # least the specified types and output formats
+    DIAGRAM_EXAMPLES = [
+      :PlantUML => "Kroki -> Julia: Hello",
+      :Graphviz => "digraph D { Hello->World }",
+      :ditaa => """
+      +--------+     +---------+
+      |        |     |         |
+      |  User  | <-> | Another |
+      |        |     |    User |
+      |        |     |         |
+      +--------+     +---------+
+      """,
+      # The specification of the diagram type is case-insensitive
+      :plantuml => "Kroki <- Julia: Hello"
+    ]
+
+    # The PNG specification defines the structure of PNG files (see
+    # http://www.libpng.org/pub/png/spec/1.2/PNG-Structure.html). The most
+    # straight-forward parts to test for are the header and the end-of-file
+    # marker (i.e. the 'IEND image trailer'), both consisting of 8 bytes
+    PNG_HEADER = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a]
+    PNG_EOF = [0x0, 0x49, 0x45, 0x4e, 0x44, 0xae, 0x42, 0x60, 0x82]
+    @testset "$(diagram_format) to PNG" for (diagram_format, specification) = DIAGRAM_EXAMPLES
+      rendered = render(Diagram{Val{diagram_format}}(specification), "png")
+
+      @test rendered[1:length(PNG_HEADER)] == PNG_HEADER
+      @test rendered[end-8:end] == PNG_EOF
+    end
+
+    @testset "$(diagram_format) to SVG" for (diagram_format, specification) = DIAGRAM_EXAMPLES
+      rendered = String(render(Diagram{Val{diagram_format}}(specification), "svg"))
+
+      @test startswith(rendered, "<?xml")
+      # Some renderers (e.g. Graphviz) include additional whitespace/newlines
+      # after the render, these should be ignored when matching
+      @test endswith(rendered, r"</svg>\s?")
     end
   end
 end
