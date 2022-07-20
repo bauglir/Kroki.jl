de6e0d37ca90729d3d3a4952a76de135c1b52393
Joris Kraak <me@joriskraak.nl>
2022-07-20 23:45:02 +0200

    test: move "`Diagram` instantiation" tests into dedicated file
    This declutters the main `runtests` file a bit and makes it easier to
    test only this particular piece of functionality (and to ignore it while
    testing other pieces of the package).

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,84 +1,10 @@
 module KrokiTest
 
-using Test: @testset, @test
+using Test: @test, @testset
 
-using Kroki: Diagram, MIMEToDiagramTypeMap, renderDiagramSupportAsMarkdown
+using Kroki: MIMEToDiagramTypeMap, renderDiagramSupportAsMarkdown
 
 @testset "Kroki" begin
-  @testset "`Diagram` instantiation" begin
-    @testset "through keyword arguments" begin
-      expected_specification = "[...]--[...]"
-      expected_type = :svgbob
-
-      diagram = Diagram(specification = expected_specification, type = expected_type)
-
-      @test diagram.specification === expected_specification
-      @test diagram.type === expected_type
-
-      @testset "optionally accepts `options`" begin
-        expected_options = Dict("key" => "value")
-
-        diagram = Diagram(
-          options = expected_options,
-          specification = expected_specification,
-          type = expected_type,
-        )
-
-        @test diagram.options === expected_options
-        @test diagram.specification === expected_specification
-        @test diagram.type === expected_type
-      end
-    end
-
-    @testset "through `type` and `specification` positional arguments" begin
-      expected_specification = "A -> B: C"
-      expected_type = :plantuml
-
-      diagram = Diagram(expected_type, expected_specification)
-
-      @test diagram.specification === expected_specification
-      @test diagram.type === expected_type
-
-      @testset "optionally accepts `options`" begin
-        expected_options = Dict("foo" => "bar")
-
-        diagram = Diagram(expected_type, expected_specification; options = expected_options)
-
-        @test diagram.options === expected_options
-        @test diagram.specification === expected_specification
-        @test diagram.type === expected_type
-      end
-    end
-
-    @testset "through `type` positional argument with keyword arguments" begin
-      @testset "providing the `path` keyword argument loads a file as the `specification" begin
-        diagram_path = joinpath(@__DIR__, "assets", "plantuml-example.puml")
-        expected_specification = read(diagram_path, String)
-
-        diagram = Diagram(:plantuml; path = diagram_path)
-
-        @test diagram.specification === expected_specification
-      end
-
-      @testset "optionally accepts `options`" begin
-        expected_options = Dict("key" => "value")
-
-        diagram =
-          Diagram(:plantuml; options = expected_options, specification = "A -> B: C")
-
-        @test diagram.options === expected_options
-      end
-
-      @testset "providing the `specification` keyword argument stores it" begin
-        expected_specification = "A -> B: C"
-
-        diagram = Diagram(:plantuml; specification = expected_specification)
-
-        @test diagram.specification === expected_specification
-      end
-    end
-  end
-
   @testset "`renderDiagramSupportAsMarkdown`" begin
     @testset "includes defined support and `image/svg+xml`" begin
       # Strip newlines that are introduced by writing the string markers on
